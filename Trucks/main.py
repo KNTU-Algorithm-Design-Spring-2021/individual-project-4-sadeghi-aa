4983d288faa2c7cae1192c1a47ff060b5836ba4f
Amirali <a.sadeghifarshi@gmail.com>
2021-07-03 03:50:06 +0430

    Final Truck

--- Trucks/main.py
+++ Trucks/main.py
@@ -3,10 +3,11 @@ from numpy import argmin
 
 
 def assign(num_trucks, weights):
+    weights.sort(reverse=True)
     assignments = [[] for _ in range(num_trucks)]
-    sum_weight = np.zeros(num_trucks)
+    sum_weight = np.zeros(num_trucks, dtype=int)
     for weight in weights:
-        choice = argmin(sum_weight + weight)
+        choice = argmin(sum_weight)
         sum_weight[choice] += weight
         assignments[choice].append(weight)
     max_weight = max(sum_weight)
@@ -15,7 +16,8 @@ def assign(num_trucks, weights):
 
 if __name__ == '__main__':
     k = 3
-    w = [8, 8, 6, 3, 2, 2]
+    w = [10, 8, 5, 2, 1]
+
     result, max_w = assign(k, w)
-    print('Max Weight is', max_w)
+    print('Max Weight with greedy algorithm is', max_w)
     print('Assignments are', result)
